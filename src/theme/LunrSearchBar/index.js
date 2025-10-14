62118bbe19514882c59f89cf2e1775734eb8a09a
Michael Perrotte <mike@mikecorp.ca>
2025-10-14 02:00:29 -0400

    feat(theme): refactor LunrSearchBar component

--- src/theme/LunrSearchBar/index.js
+++ src/theme/LunrSearchBar/index.js
@@ -3,145 +3,156 @@
  * by Facebook, Inc., licensed under the MIT license.
  */
 
-import React, { useState, useRef, useCallback } from 'react';
+import React, {
+  createElement,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  Fragment,
+} from 'react';
+import { createRoot } from 'react-dom/client';
 import classnames from 'classnames';
 import * as lunr from 'lunr';
+import { autocomplete } from '@algolia/autocomplete-js';
 
 import { useHistory } from '@docusaurus/router';
 import { usePluginData } from '@docusaurus/useGlobalData';
 
 import useDocusaurusDocsVersion from '../hooks/version';
 
-import './input.css';
-import './autocomplete.css';
+import '@algolia/autocomplete-theme-classic';
+
+const defaultHeaderTemplate = () => {
+  return null;
+};
+
+const defaultItemTemplate = (params) => {
+  const { item, components } = params;
+
+  return (
+    <div className="aa-ItemWrapper">
+      <div className="aa-ItemContent">
+        <div className="aa-ItemContentBody">
+          <div className="aa-ItemContentTitle">
+            <components.Highlight hit={item} attribute="title" />
+          </div>
+          <div className="aa-ItemContentDescription">url: {item.route}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const defaultNoResultsTemplate = () => {
+  return 'No results found.';
+};
+
+const defaultTransform = ({ result, documents }) => {
+  const document = documents.find((document) => document.route === result.ref);
+
+  return { ...document, result };
+};
 
 const Search = (props) => {
-  const [indexState, setIndexState] = useState('empty');
-  const searchBarRef = useRef(null);
-  const history = useHistory();
+  const {
+    maxResults = 8,
+    headerTemplate = defaultHeaderTemplate,
+    itemTemplate = defaultItemTemplate,
+    noResultsTemplate = defaultNoResultsTemplate,
+    components = null,
+    placeholder = 'Search',
+    transform = defaultTransform,
+  } = props;
+
+  const containerRef = useRef(null);
+  const panelRootRef = useRef(null);
+  const rootRef = useRef(null);
 
   const currentVersion = useDocusaurusDocsVersion();
   const pluginData = usePluginData('docusaurus-plugin-lunr');
 
   const { searchIndex } = pluginData;
+  const { index, documents } = searchIndex;
 
-  const loadIndex = async () => {
-    if (indexState !== 'empty') {
+  const source = useMemo(() => lunr.Index.load(index), [index]);
+
+  useEffect(() => {
+    if (!containerRef.current) {
       return;
     }
-    setIndexState('loading');
 
-    const indexLoaded = (index, documents, autoComplete) => {
-      autoComplete.default.noConflict();
-
-      autoComplete
-        .default(
-          searchBarRef.current,
+    const search = autocomplete({
+      container: containerRef.current,
+      placeholder,
+      insights: false,
+      ...(components && { components }),
+      getSources() {
+        return [
           {
-            hint: false,
-            autoselect: true,
-            cssClasses: {
-              root: 'd-s-l-a',
-            },
-          },
-          [
-            {
-              source: (input, cb) => {
-                const terms = input
-                  .split(' ')
-                  .map((each) => each.trim().toLowerCase())
-                  .filter((each) => each.length > 0);
-                const results = index
-                  .query((query) => {
-                    if (currentVersion) {
-                      query.term(currentVersion, {
-                        fields: ['version'],
-                        presence: lunr.Query.presence.REQUIRED,
-                      });
-                    }
-                    query.term(terms, { fields: ['title', 'content'] });
-                    query.term(terms, {
-                      fields: ['title', 'content'],
-                      wildcard: lunr.Query.wildcard.TRAILING,
+            sourceId: 'index',
+            getItems({ query: inputQuery }) {
+              const terms = inputQuery
+                .split(' ')
+                .map((each) => each.trim().toLowerCase())
+                .filter((each) => each.length > 0);
+
+              const results = source
+                .query((query) => {
+                  if (currentVersion) {
+                    query.term(currentVersion, {
+                      fields: ['version'],
+                      presence: lunr.Query.presence.REQUIRED,
                     });
-                  })
-                  .slice(0, 8);
-                cb(results);
-              },
-              templates: {
-                suggestion: function (suggestion) {
-                  const document = documents.find(
-                    (document) => document.route === suggestion.ref,
-                  );
-                  return autoComplete.default.escapeHighlightedString(
-                    document.title,
-                  );
-                },
-                empty: () => {
-                  return 'no results';
-                },
-              },
+                  }
+                  query.term(terms, { fields: ['title', 'content'] });
+                  query.term(terms, {
+                    fields: ['title', 'content'],
+                    wildcard: lunr.Query.wildcard.TRAILING,
+                  });
+                })
+                .map((result) => transform({ result, documents }))
+                .slice(0, maxResults);
+
+              return results;
             },
-          ],
-        )
-        .on(
-          'autocomplete:selected',
-          function (event, suggestion, dataset, context) {
-            history.push(suggestion.ref);
+            getItemUrl({ item }) {
+              return item.route;
+            },
+            getItemInputValue({ item }) {
+              return item.title;
+            },
+            onSelect(params) {
+              const { itemUrl, navigator } = params;
+              navigator.navigate({ itemUrl });
+            },
+            templates: {
+              header: headerTemplate,
+              item: itemTemplate,
+              noResults: noResultsTemplate,
+            },
           },
-        );
-      setIndexState('done');
+        ];
+      },
+      renderer: { createElement, Fragment, render: () => {} },
+      render({ children }, root) {
+        if (!panelRootRef.current || rootRef.current !== root) {
+          rootRef.current = root;
+
+          panelRootRef.current?.unmount();
+          panelRootRef.current = createRoot(root);
+        }
+
+        panelRootRef.current.render(children);
+      },
+    });
+
+    return () => {
+      search.destroy();
     };
+  }, [containerRef.current]);
 
-    const autoComplete = await import('autocomplete.js');
-
-    const { documents, index } = searchIndex;
-    indexLoaded(lunr.Index.load(index), documents, autoComplete);
-  };
-
-  const toggleSearchIconClick = useCallback(() => {
-    loadIndex();
-
-    if (indexState === 'done') {
-      searchBarRef.current.focus();
-    }
-
-    props.handleSearchBarToggle(!props.isSearchBarExpanded);
-  }, [props.isSearchBarExpanded, indexState]);
-
-  const handleSearchInputBlur = useCallback(() => {
-    props.handleSearchBarToggle(!props.isSearchBarExpanded);
-  }, [props.isSearchBarExpanded]);
-
-  return (
-    <div className="navbar__search" key="search-box">
-      <span
-        aria-label="expand searchbar"
-        role="button"
-        className={classnames('search-icon', {
-          'search-icon-hidden': props.isSearchBarExpanded,
-        })}
-        onClick={toggleSearchIconClick}
-        onKeyDown={toggleSearchIconClick}
-        tabIndex={0}
-      />
-      <input
-        id="search_input_react"
-        type="search"
-        placeholder="Search"
-        aria-label="Search"
-        className={classnames(
-          'navbar__search-input',
-          { 'search-bar-expanded': props.isSearchBarExpanded },
-          { 'search-bar': !props.isSearchBarExpanded },
-        )}
-        onMouseOver={loadIndex}
-        onFocus={loadIndex}
-        onBlur={handleSearchInputBlur}
-        ref={searchBarRef}
-      />
-    </div>
-  );
+  return <div ref={containerRef} />;
 };
 
 export default Search;
